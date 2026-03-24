579bc3329227bb66302243b341ebc0550fc1c395
Harsh Raj Singh <harshrajsingh246@gmail.com>
2026-03-24 23:58:33 +0530

    Attributes added in Data modelling of users

--- Backend/app.js
+++ Backend/app.js
@@ -1,6 +1,13 @@
 const express = require('express')
 const app = express()
+const cookieParser = require('cookie-parser')
+
+
 require('dotenv').config()
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(cookieParser())
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
--- Backend/models/user.models.js
+++ Backend/models/user.models.js
@@ -6,19 +6,28 @@ const userSchema = new mongoose.Schema({
         required:[true,"Username is required"],
         unique:[true,"Username already exists"],
         min:[5,"Username must be at least 5 characters long"],
-        max:[20,"Username must be at most 20 characters long"]
+        max:[20,"Username must be at most 20 characters long"],
+        trim:true
     },
     email: {
         type:String,
         required:[true,"Email is required"],
         unique:[true,"Email already exists"],
         lowercase:true,
-        // trim:true
     },
     password: {
         type:String,
         required:[true,"Password is required"],
-        min:[8,"Password must be at least 8 characters long"]
+        min:[6,"Password must be at least 6 characters long"],
+        max:[20,"Password must be at most 20 characters long"]
+    },
+    level:{
+        type:Number,
+        default:1
+    },
+    xp:{
+        type:Number,
+        default:0
     },
     // avatar:{
     //     type:String,
